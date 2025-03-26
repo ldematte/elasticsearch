d635230d725046fd197eaf37e52614cade1b4981
ldematte <lorenzo.dematte@elastic.co>
2025-03-26 15:18:39 +0100

    small benchmark tweak

--- benchmarks/src/main/java/org/elasticsearch/benchmark/xcontent/JsonParserBenchmark.java
+++ benchmarks/src/main/java/org/elasticsearch/benchmark/xcontent/JsonParserBenchmark.java
@@ -33,11 +33,11 @@ import java.util.concurrent.TimeUnit;
 /**
  * Benchmarks for testing and comparing the performance of JsonXContentParser implementation(s)
  */
-@Fork(1)
+@Fork(value = 1, jvmArgsPrepend = { "--add-modules=jdk.incubator.vector" })
 @Warmup(iterations = 1)
 @Measurement(iterations = 2)
-@BenchmarkMode({ Mode.Throughput, Mode.AverageTime })
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 @OperationsPerInvocation(25000)
 @State(Scope.Benchmark)
 public class JsonParserBenchmark {
